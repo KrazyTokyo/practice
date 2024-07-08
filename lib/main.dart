b9dc585b815ac18f6656719e1456e9256943992c
KrazyTokyo <chlarenzterrones@gmail.com>
2024-07-08 18:52:03 +0800

    set up firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:practice_with_git/screens/screens.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyAqyVQ2zCXzpGgqWBOsDW_dax2geWMr3Os",
+            appId: "1:345836628011:web:ce6e8a399a170d30f7a26e",
+            messagingSenderId: "345836628011",
+            projectId: "practice-firebase-4b7bc"));
+  }
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
